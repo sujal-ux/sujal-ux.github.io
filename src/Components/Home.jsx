d0c044e0dd3810cbfebfa3a4ba7a4198c49c93f6
sujal-ux <sumo12082000@gmail.com>
2023-01-22 01:19:53 +0530

    New Home Page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,30 +3,28 @@ import { ThemeContext } from "../themeProvider";
 import { motion } from "framer-motion";
 import Typical from "react-typical";
 
-
 export const Home = () => {
   const theme = useContext(ThemeContext);
   const darkmode = theme.state.darkmode;
   const [stage, changeStage] = useState(1);
   const per = useRef();
 
-  useEffect(()=>{
+  useEffect(() => {
     var id = setInterval(() => {
       var tmp = per.current.innerText;
       var cur = parseInt(tmp);
       cur = cur + 1;
       per.current.innerText = cur;
-      if(cur>=99) {
+      if (cur >= 99) {
         clearInterval(id);
         changeStage(3);
       }
     }, 60);
-    
-  }, [])
+  }, []);
 
   return (
     <>
-      <div
+      {/* <div
         style={
           darkmode
             ? {
@@ -106,6 +104,85 @@ export const Home = () => {
             </div>
           </div>
         </main>
+      </div> */}
+
+      <div className="home-box">
+        <div className="home-name">
+          <video autoPlay muted loop>
+            <source
+              src="https://mdbootstrap.com/img/video/Sail-Away.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support HTML video.
+          </video>
+          <h2>Sujal</h2>
+        </div>
+        <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
+          <span className="block text-blue-500 font-extrabold z-0 lg:inline">
+            <Typical
+              steps={[
+                "Software Engineer",
+                2000,
+                "Competitive Programmer",
+                2000,
+                "Web Developer",
+                2000,
+              ]}
+              loop={Infinity}
+            />
+          </span>
+        </h1>
+      <div className="home-data">
+        <div className="console-box">
+          <div className="console-head">
+            <i className="console-head-dot"></i>
+            <i className="console-head-dot"></i>
+            <i className="console-head-dot"></i>
+          </div>
+          <div className="console-body">
+            {stage >= 0 ? (
+              <div className="console-body-row" id="0">
+                <i className="fa fa-dollar text-blue-200"></i>
+                <div className="console-body-text text-blue-200">
+                  npm install @love.js
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )}
+            {stage >= 1 ? (
+              <div className="console-body-row" id="1">
+                <i className="fa fa-angle-right text-orange-400"></i>
+                <div className="console-body-text text-orange-400">
+                  installing...<span ref={per}>0</span>%
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )}
+            {stage >= 2 ? (
+              <div className="console-body-row" id="2">
+                <i className="fa fa-angle-right text-red-400"></i>
+                <div className="console-body-text text-red-400">
+                  error: love not found
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )}
+            {stage >= 3 ? (
+              <div className="console-body-row" id="3">
+                <i className="text-blue-200">#</i>
+                <div className="console-body-text text-blue-200">
+                  try: npm install -g @gf.js
+                </div>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
+        </div>
+      </div>
       </div>
     </>
   );
